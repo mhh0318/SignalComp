e8f014857ec25404c0dd8eb02f97cf37fbcfd103
mmh0318 <mercihu0318@gmail.com>
2020-09-05 16:56:49 +0800

    Initial commit

---
+++ baseline_pytorch/Model_define_pytorch.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+"""An Implement of an autoencoder with pytorch.
+This is the template code for 2020 NIAC https://naic.pcl.ac.cn/.
+The code is based on the sample code with tensorflow for 2020 NIAC and it can only run with GPUS.
+If you have any questions, please contact me with https://github.com/xufana7/AutoEncoder-with-pytorch
+Author, Fan xu Aug 2020
+"""
+import numpy as np
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+
+# This part implement the quantization and dequantization operations.
+# The output of the encoder must be the bitstream.
+def Num2Bit(Num, B):
+    Num_ = Num.type(torch.uint8)
+
+    def integer2bit(integer, num_bits=B * 2):
+        dtype = integer.type()
+        exponent_bits = -torch.arange(-(num_bits - 1), 1).type(dtype)
+        exponent_bits = exponent_bits.repeat(integer.shape + (1,))
+        out = integer.unsqueeze(-1) // 2 ** exponent_bits
+        return (out - (out % 1)) % 2
+
+    bit = integer2bit(Num_)
+    bit = (bit[:, :, B:]).reshape(-1, Num_.shape[1] * B)
+    return bit.type(torch.float32)
+
+
+def Bit2Num(Bit, B):
+    Bit_ = Bit.type(torch.float32)
+    Bit_ = torch.reshape(Bit_, [-1, int(Bit_.shape[1] / B), B])
+    num = torch.zeros(Bit_[:, :, 1].shape).cuda()
+    for i in range(B):
+        num = num + Bit_[:, :, i] * 2 ** (B - 1 - i)
+    return num
+
+
+class Quantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = torch.round(x * step - 0.5)
+        out = Num2Bit(out, B)
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of constant arguments to forward must be None.
+        # Gradient of a number is the sum of its four bits.
+        b, _ = grad_output.shape
+        grad_num = torch.sum(grad_output.reshape(b, -1, ctx.constant), dim=2)
+        return grad_num, None
+
+
+class Dequantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = Bit2Num(x, B)
+        out = (out + 0.5) / step
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of non-Tensor arguments to forward must be None.
+        # repeat the gradient of a Num for four time.
+        b, c = grad_output.shape
+        grad_bit = grad_output.repeat(1, 1, ctx.constant)
+        return torch.reshape(grad_bit, (-1, c * ctx.constant)), None
+
+
+class QuantizationLayer(nn.Module):
+
+    def __init__(self, B):
+        super(QuantizationLayer, self).__init__()
+        self.B = B
+
+    def forward(self, x):
+        out = Quantization.apply(x, self.B)
+        return out
+
+
+class DequantizationLayer(nn.Module):
+
+    def __init__(self, B):
+        super(DequantizationLayer, self).__init__()
+        self.B = B
+
+    def forward(self, x):
+        out = Dequantization.apply(x, self.B)
+        return out
+
+
+# create your own Encoder
+class Encoder(nn.Module):
+    B = 4
+
+    def __init__(self, feedback_bits):
+        super(Encoder, self).__init__()
+
+
+# create your own Decoder
+class Decoder(nn.Module):
+    B = 4
+
+    def __init__(self, feedback_bits):
+        super(Decoder, self).__init__()
+
+
+# Note: Do not modify following class and keep it in your submission.
+# feedback_bits is 128 by default.
+class AutoEncoder(nn.Module):
+
+    def __init__(self, feedback_bits):
+        super(AutoEncoder, self).__init__()
+        self.encoder = Encoder(feedback_bits)
+        self.decoder = Decoder(feedback_bits)
+
+    def forward(self, x):
+        feature = self.encoder(x)
+        out = self.decoder(feature)
+        return out
+
+
+def NMSE(x, x_hat):
+    x_real = np.reshape(x[:, :, :, 0], (len(x), -1))
+    x_imag = np.reshape(x[:, :, :, 1], (len(x), -1))
+    x_hat_real = np.reshape(x_hat[:, :, :, 0], (len(x_hat), -1))
+    x_hat_imag = np.reshape(x_hat[:, :, :, 1], (len(x_hat), -1))
+    x_C = x_real - 0.5 + 1j * (x_imag - 0.5)
+    x_hat_C = x_hat_real - 0.5 + 1j * (x_hat_imag - 0.5)
+    power = np.sum(abs(x_C) ** 2, axis=1)
+    mse = np.sum(abs(x_C - x_hat_C) ** 2, axis=1)
+    nmse = np.mean(mse / power)
+    return nmse
+
+
+def Score(NMSE):
+    score = 1 - NMSE
+    return score
+
+
+# dataLoader
+class DatasetFolder(Dataset):
+
+    def __init__(self, matData):
+        self.matdata = matData
+
+    def __getitem__(self, index):
+        return self.matdata[index]
+
+    def __len__(self):
+        return self.matdata.shape[0]
